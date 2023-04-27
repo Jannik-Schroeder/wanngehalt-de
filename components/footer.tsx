448287338f72420345db362945675fb54fbdbf25
Jannik Schroeder <jannik.schroeder05@gmail.com>
2023-04-27 08:00:04 +0000

    init

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function Footer() {
   return (
        <footer className="text-white py-4 text-center bg-gray-900">
-        <div className="text-sm">by <a className="underline font-bold" href="https://dwag.me/" rel="dofollow">dwag</a></div>
+        <div className="text-sm">created by <a className="underline font-bold" href="https://dwag.me/" rel="dofollow">dwag</a> & maintained by <a className="underline font-bold" href="https://jannik.schroeder.is/" rel="dofollow">schroeder</a></div>
       </footer>
   )
 }
