9ef0180df396382115856a7b6b6c674242ff5cb5
Jannik Schroeder <jannik.schroeder05@gmail.com>
2023-04-27 11:14:50 +0000

    initial changfes

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <>
     <Head>
       <title>Wann Gehalt?</title>
-      <meta name="description" content="Erfahren Sie mit wanngehalt.at, wann Sie Ihr nächstes Gehalt erhalten." />
+      <meta name="description" content="Erfahren Sie mit wanngehalt.de, wann Sie Ihr nächstes Gehalt erhalten." />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="Wann Gehalt?" />
-      <meta property="og:description" content="Erfahren Sie mit wanngehalt.at, wann Sie Ihr nächstes Gehalt erhalten." />
+      <meta property="og:description" content="Erfahren Sie mit wanngehalt.de, wann Sie Ihr nächstes Gehalt erhalten." />
     </Head>
       <Countdown />
       <Footer />
